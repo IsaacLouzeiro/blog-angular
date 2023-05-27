dcf4f8764f9f1a795d921423c7e4416376cbdcb1
IsaacLouzeiro <isaacelias1110@gmail.com>
2023-05-27 18:19:16 -0300

    criando estrutura basica dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { CommonCardComponent } from './components/common-card/common-card.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    MainCardComponent,
+    CommonCardComponent,
+    UserCardComponent,
+    AsideComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/common-card/common-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-common-card',
+  templateUrl: './common-card.component.html',
+  styleUrls: ['./common-card.component.scss']
+})
+export class CommonCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
